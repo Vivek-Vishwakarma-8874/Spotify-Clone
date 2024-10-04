adb47a58214aa16febe260688ccf5ae55691cd94
Vivek-Vishwakarma-8874 <166617357+Vivek-Vishwakarma-8874@users.noreply.github.com>
2024-10-04 16:48:04 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ function convertSecondsToMinutes(seconds) {
 }
 async function getSongs(folder) {
     currFolder = folder
-    let a = await fetch(`http://127.0.0.1:3000/${folder}/`)
+    let a = await fetch(`https://github.com/Vivek-Vishwakarma-8874/Spotify-Clone/tree/main/Songs/${folder}/`)
     let response = await a.text()
     // console.log(response)
     let div = document.createElement("div")
